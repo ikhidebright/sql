6df5d7d9669e9f51553477601e2e930891074cc4
Ikhide Bright <47560132+ikhidebright@users.noreply.github.com>
2020-03-31 17:50:58 +0100

    Create queries.sql

---
+++ queries.sql
@@ -0,0 +1,6 @@
+Ques 2
+SELECT * FROM order_items WHERE order_id = 6 && (quantity * unit_price) > 30;
+
+Ques 3
+SELECT * FROM products WHERE quantity_in_stock = 49 || quantity_in_stock = 38 || quantity_in_stock = 72;
+
